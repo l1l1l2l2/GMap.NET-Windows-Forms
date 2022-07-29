dff61791810f5abc204270128f07c6bee3059b5f
denis <okposhka09@gmail.com>
2022-07-30 00:19:27 +0700

    add GMap.NET

--- View/MainForm.cs
+++ View/MainForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,21 @@ namespace View
         {
             InitializeComponent();
         }
+
+        private void gMapControl_Load(object sender, EventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; //выбор подгрузки карты – онлайн или из ресурсов
+            gMapControl.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance; //какой провайдер карт используется (в нашем случае гугл) 
+            gMapControl.MinZoom = 2; //минимальный зум
+            gMapControl.MaxZoom = 16; //максимальный зум
+            gMapControl.Zoom = 4; // какой используется зум при открытии
+            gMapControl.Position = new GMap.NET.PointLatLng(66.4169575018027, 94.25025752215694);// точка в центре карты при открытии (центр России)
+            gMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter; // как приближает (просто в центр карты или по положению мыши)
+            gMapControl.CanDragMap = true; // перетаскивание карты мышью
+            gMapControl.DragButton = MouseButtons.Left; // какой кнопкой осуществляется перетаскивание
+            gMapControl.ShowCenter = false; //показывать или скрывать красный крестик в центре
+            gMapControl.ShowTileGridLines = false; //показывать или скрывать тайлы
+        }
+        
     }
 }
