f14091639eec43ed1c29945b96820f06b7e0c191
denis <okposhka09@gmail.com>
2022-07-30 01:10:00 +0700

    Add UnitOfWork

---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Data
+{
+    public class UnitOfWork
+    {
+        public Repository<Coordinate> Coordinates { get; private set; }
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public UnitOfWork()
+        {
+            Coordinates = new Repository<Coordinate>(_connectionString);
+        }
+        
+    }
+}
